b1d59830bd475c5678d51198e54c56b1c29becbc
Antizore <2906pascal@gmail.com>
2025-09-15 22:52:51 +0200

    revert because records dont work well for entities with relations

--- src/main/java/com/example/CableERP/entity/Product.java
+++ src/main/java/com/example/CableERP/entity/Product.java
@@ -2,23 +2,62 @@ package com.example.CableERP.entity;
 
 
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "product")
-public record Product(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        Long id,
-        String name,
-        String description,
-        @OneToMany(mappedBy = "product")
-        List<BillOfMaterials> billOfMaterialsList
-) { }
+public class Product {
+    public Product(Long id, String name, String description, List<BillOfMaterials> billOfMaterialsList) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.billOfMaterialsList = billOfMaterialsList;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "product")
+    private List<BillOfMaterials> billOfMaterialsList = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<BillOfMaterials> getBillOfMaterialsList() {
+        return billOfMaterialsList;
+    }
+
+    public void setBillOfMaterialsList(List<BillOfMaterials> billOfMaterialsList) {
+        this.billOfMaterialsList = billOfMaterialsList;
+    }
 
 
-
-
-
-
-
+}
