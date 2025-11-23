e33ff4168289aff808ef1c1e377b3527bfdc0f33
Antizore <2906pascal@gmail.com>
2025-11-23 23:08:52 +0100

    calculate net requirements

--- src/main/java/com/example/CableERP/MRP/Service.java
+++ src/main/java/com/example/CableERP/MRP/Service.java
@@ -3,6 +3,8 @@ package com.example.CableERP.MRP;
 
 import com.example.CableERP.BillOfMaterials.BillOfMaterials;
 import com.example.CableERP.Component.Component;
+import com.example.CableERP.Component.ComponentRepository;
+import com.example.CableERP.Inventory.InventoryRepository;
 import com.example.CableERP.WorkOrder.WorkOrder;
 import com.example.CableERP.WorkOrder.WorkOrderRepository;
 import com.example.CableERP.WorkOrder.WorkOrderService;
@@ -16,9 +18,12 @@ public class Service {
 
 
     private final WorkOrderRepository workOrderRepository;
+    private final InventoryRepository inventoryRepository;
 
-    public Service(WorkOrderRepository workOrderRepository){
+    public Service(WorkOrderRepository workOrderRepository,InventoryRepository inventoryRepository){
         this.workOrderRepository = workOrderRepository;
+        this.inventoryRepository = inventoryRepository;
+
     }
 
 
@@ -26,37 +31,44 @@ public class Service {
 
     public void mrpRun(){
         List<WorkOrder> listOfPlannedWorkOrders = workOrderRepository.findByStatus(WorkOrderStatus.PLANNED);
-
-
-
+        HashMap<Component, Double> componentsRequired = calculateGrossRequirements(listOfPlannedWorkOrders);
+        System.out.println(calculateNetRequirements(componentsRequired));
 
 
     }
 
 
     //TODO liczymy zapotrzebowanie na wszystkie komponenty
-    private HashMap<String,Double> calculateGrossRequirements(List<WorkOrder> listOfPlannedWorkOrders){
-        HashMap<String, Double> componentsNeeded = new HashMap<>();
+    private HashMap<Component,Double> calculateGrossRequirements(List<WorkOrder> listOfPlannedWorkOrders){
+        HashMap<Component, Double> componentsNeeded = new HashMap<>();
         for(WorkOrder workOrder : listOfPlannedWorkOrders){
             List<BillOfMaterials> billOfMaterialsList = workOrder.getProduct().getBillOfMaterialsList();
             for (BillOfMaterials bill : billOfMaterialsList){
-                componentsNeeded.computeIfAbsent(bill.getComponent().getName(), value -> bill.getQty() * workOrder.getQty());
-                componentsNeeded.computeIfPresent(bill.getComponent().getName(), (key,oldVal) -> (oldVal += bill.getQty() * workOrder.getQty()) );
+                componentsNeeded.computeIfAbsent(bill.getComponent(), value -> bill.getQty() * workOrder.getQty());
+                componentsNeeded.computeIfPresent(bill.getComponent(), (key,oldVal) -> (oldVal += bill.getQty() * workOrder.getQty()) );
             }
         }
         return componentsNeeded;
     }
 
-    /*
-    TODO
-     liczymy zapotrzebowanie net = gross - qty_available - qty_reserved i sprawdzamy czy są braki
-     wynikiem ma być tylko lista komponentów z brakami
-     { component_id: 12, missing: 80 },
-     { component_id: 7, missing: 15 }
-     */
-    private void calculateNetRequirements(){
+    private HashMap<Component,Double> calculateNetRequirements(HashMap<Component, Double> componentsRequired){
 
+        HashMap<Component, Double> missingComponents = new HashMap<>();
+
+        componentsRequired.forEach(
+                (k,v) ->
+                {
+                    if(v < inventoryRepository.findByComponentId(k.getId()).getQtyAvailable() + inventoryRepository.findByComponentId(k.getId()).getQtyReserved())
+                    {
+                        missingComponents.computeIfPresent(k, (k1,v1) -> (v1 += v));
+                        missingComponents.computeIfAbsent(k, v1 -> v);
+                    }
+                }
+        );
+        return missingComponents;
     }
+
+
 
     //TODO
     private void planWorkOrders(){
