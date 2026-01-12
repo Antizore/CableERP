2b0732806372638dcf1f61543d5f8cdbe25e8ca7
Antizore <2906pascal@gmail.com>
2026-01-12 23:01:02 +0100

    simple cleaning

--- src/main/java/com/example/CableERP/MRP/Service.java
+++ src/main/java/com/example/CableERP/MRP/Service.java
@@ -14,17 +14,18 @@ import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Service
 public class Service {
+
     private final WorkOrderRepository workOrderRepository;
     private final InventoryRepository inventoryRepository;
     private final BillOfMaterialsRepository billOfMaterialsRepository;
 
-    public Service(WorkOrderRepository workOrderRepository,InventoryRepository inventoryRepository, BillOfMaterialsRepository billOfMaterialsRepository){
+    public Service(WorkOrderRepository workOrderRepository, InventoryRepository inventoryRepository, BillOfMaterialsRepository billOfMaterialsRepository) {
         this.workOrderRepository = workOrderRepository;
         this.inventoryRepository = inventoryRepository;
         this.billOfMaterialsRepository = billOfMaterialsRepository;
     }
 
-    public MrpRunResponse mrpRun(){
+    public MrpRunResponse mrpRun() {
         List<WorkOrder> listOfPlannedWorkOrders = workOrderRepository.findByStatus(WorkOrderStatus.PLANNED);
         List<ComponentMRPDTO> grossComponentsRequired = calculateGrossRequirements(listOfPlannedWorkOrders);
         List<RequirementsDTO> componentsToProduce = calculateNetRequirements(grossComponentsRequired).get(1);
@@ -32,24 +33,23 @@ public class Service {
 
 
         return new MrpRunResponse(grossComponentsRequired.stream().map(
-                 componentMRPDTO ->  new RequirementsDTO(componentMRPDTO.getId(),componentMRPDTO.getQty())
-        ).collect(Collectors.toList()),componentsToBuy,componentsToProduce);
+                componentMRPDTO -> new RequirementsDTO(componentMRPDTO.getId(), componentMRPDTO.getQty())
+        ).collect(Collectors.toList()), componentsToBuy, componentsToProduce);
 
     }
 
-    private List<ComponentMRPDTO> calculateGrossRequirements(List<WorkOrder> listOfPlannedWorkOrders){
+    private List<ComponentMRPDTO> calculateGrossRequirements(List<WorkOrder> listOfPlannedWorkOrders) {
         List<ComponentMRPDTO> componentsNeeded = new ArrayList<>();
 
-        for(WorkOrder workOrder : listOfPlannedWorkOrders){
+        for (WorkOrder workOrder : listOfPlannedWorkOrders) {
             List<BillOfMaterials> billOfMaterialsList = workOrder.getProduct().getBillOfMaterialsList();
-            for (BillOfMaterials bill : billOfMaterialsList){
+            for (BillOfMaterials bill : billOfMaterialsList) {
                 //TODO: ŁADNIEJ
                 ComponentMRPDTO componentMRPDTO = new ComponentMRPDTO(bill.getComponent().getId(), bill.getComponent().getName(), bill.getQty());
                 ComponentMRPDTO componentMRPDTO1 = componentsNeeded.stream().filter(a -> a == componentMRPDTO).findFirst().orElse(null);
-                if(componentMRPDTO1 == null){
+                if (componentMRPDTO1 == null) {
                     componentsNeeded.add(componentMRPDTO);
-                }
-                else {
+                } else {
                     componentMRPDTO1.setQty(componentMRPDTO1.getQty() + bill.getQty());
                 }
             }
@@ -58,7 +58,7 @@ public class Service {
     }
 
     //TODO: NA PEWNO TAK CHCESZ?
-    private List<List<RequirementsDTO>> calculateNetRequirements(List<ComponentMRPDTO> componentsRequired){
+    private List<List<RequirementsDTO>> calculateNetRequirements(List<ComponentMRPDTO> componentsRequired) {
 
         List<RequirementsDTO> missingComponentsToBuy = new ArrayList<>();
         List<RequirementsDTO> missingComponentsToProduce = new ArrayList<>();
@@ -67,20 +67,16 @@ public class Service {
         result.add(missingComponentsToProduce);
 
 
-
         componentsRequired.forEach(
                 (k) ->
                 {
                     {
-                        if(k.getQty() < inventoryRepository.findByComponentId(k.getId()).getQtyAvailable() + inventoryRepository.findByComponentId(k.getId()).getQtyReserved())
-                        {
+                        if (k.getQty() < inventoryRepository.findByComponentId(k.getId()).getQtyAvailable() + inventoryRepository.findByComponentId(k.getId()).getQtyReserved()) {
                             //TODO: ŁADNIEJ
-                            if(billOfMaterialsRepository.findAllByProduct_Name(k.getName()) == null || billOfMaterialsRepository.findAllByProduct_Name(k.getName()).isEmpty())
-                            {
-                                missingComponentsToBuy.add(new RequirementsDTO(k.getId(),k.getQty()));
-                            }
-                            else{
-                                missingComponentsToProduce.add(new RequirementsDTO(k.getId(),k.getQty()));
+                            if (billOfMaterialsRepository.findAllByProduct_Name(k.getName()) == null || billOfMaterialsRepository.findAllByProduct_Name(k.getName()).isEmpty()) {
+                                missingComponentsToBuy.add(new RequirementsDTO(k.getId(), k.getQty()));
+                            } else {
+                                missingComponentsToProduce.add(new RequirementsDTO(k.getId(), k.getQty()));
                             }
                         }
                     }
@@ -88,8 +84,6 @@ public class Service {
         );
         return result;
     }
-
-
 
 
 }
