70295387e5d9b39a074abf8815cc4620fb0a4d5c
Antizore <2906pascal@gmail.com>
2026-01-19 23:25:36 +0100

    making logic for estimating date for all components

--- src/main/java/com/example/CableERP/MRP/EstimateOrderDate.java
+++ src/main/java/com/example/CableERP/MRP/EstimateOrderDate.java
@@ -1,4 +1,69 @@
 package com.example.CableERP.MRP;
 
+import com.example.CableERP.BillOfMaterials.BillOfMaterialsDTO;
+import com.example.CableERP.BillOfMaterials.BillOfMaterialsForEstimate;
+import com.example.CableERP.Component.Component;
+import com.example.CableERP.Component.ComponentDTO;
+import com.example.CableERP.Customer.CustomerOrder.OrderItem;
+import com.example.CableERP.Inventory.Inventory;
+import com.example.CableERP.Inventory.InventoryRepository;
+import com.example.CableERP.Procurement.ProcurementRepository;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 public class EstimateOrderDate {
+
+    private final InventoryRepository inventoryRepository;
+
+    public EstimateOrderDate(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public Double estimate(List<OrderItem> orderItemList) {
+
+        Map<Component, Double> summaricOfComponentNeeds = new HashMap<>();
+        for (OrderItem orderItem : orderItemList) {
+
+            // prod time orderItem.getProduct().getMinutesToProduceOnePiece() * orderItem.getQty();
+
+            List<BillOfMaterialsForEstimate> listOfNeededComponents = orderItem.getProduct().getBillOfMaterialsList().
+                    stream().map(
+                    b -> new BillOfMaterialsForEstimate(b.getQty(), new Component(b.getComponent().getName(),
+                            b.getComponent().getUnit(),b.getComponent().getCostPerUnit()))
+            ).toList();
+
+            for (BillOfMaterialsForEstimate bill : listOfNeededComponents) {
+                summaricOfComponentNeeds.putIfAbsent(bill.component(), bill.qty() * orderItem.getQty());
+                summaricOfComponentNeeds.computeIfPresent(bill.component(), (k, v) -> v += bill.qty()
+                        * orderItem.getQty());
+            }
+        }
+
+        // let's check with inventory how our components are doing and if we have to wait for some of them
+        // this is a map containing component and its availability in days
+        // 0 means that there are enough components to start production
+        // value > 0 means that you need make order for procurement and need to wait for shipment (lead time days)
+        Map<Component, Double> componentAvailability = new HashMap<>();
+
+        summaricOfComponentNeeds.forEach(
+                (k,v) -> {
+                    Double availableQty = inventoryRepository.findByComponentId(k.getId()).getQtyAvailable();
+                    if(v > availableQty) {
+                        componentAvailability.put(k,k.getProcurement().getLeadTimeDays());
+                    }
+                    else {
+                        componentAvailability.put(k,0.);
+                    }
+                }
+        );
+        //you can start only when all components are available so need to know te maximum waiting time
+        return Collections.max(componentAvailability.values());
+    }
+
+
 }
