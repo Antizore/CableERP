77946f924dd2cdfc30bcf2b7f68a057f2b042365
Antizore <2906pascal@gmail.com>
2026-01-25 18:07:18 +0100

    adding new query to get the latest timestamp

--- src/main/java/com/example/CableERP/WorkOrder/WorkOrderRepository.java
+++ src/main/java/com/example/CableERP/WorkOrder/WorkOrderRepository.java
@@ -1,13 +1,20 @@
 package com.example.CableERP.WorkOrder;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface WorkOrderRepository extends JpaRepository<WorkOrder, Long> {
 
     List<WorkOrder> findByStatus(WorkOrderStatus status);
+
+    @Query(
+            "SELECT MAX(w.plannedEndAt) FROM WorkOrder w"
+    )
+    Timestamp findLastPlanned();
 
 }
