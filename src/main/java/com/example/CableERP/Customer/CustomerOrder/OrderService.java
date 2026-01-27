5778d2f284ef873a045db795918e05ad61e4b0ff
Antizore <2906pascal@gmail.com>
2026-01-27 21:55:57 +0100

    big overhual that is followed by change in bussiness logic

--- src/main/java/com/example/CableERP/Customer/CustomerOrder/OrderService.java
+++ src/main/java/com/example/CableERP/Customer/CustomerOrder/OrderService.java
@@ -4,11 +4,12 @@ package com.example.CableERP.Customer.CustomerOrder;
 import com.example.CableERP.Common.Exception.IllegalOperationException;
 import com.example.CableERP.Common.Exception.WrongValueException;
 import com.example.CableERP.Customer.CustomerRepository;
+import com.example.CableERP.Inventory.InventoryRepository;
+import com.example.CableERP.MRP.Estimation;
 import com.example.CableERP.Product.ProductCreateDTO;
 import com.example.CableERP.Product.ProductRepository;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
-
 import java.util.*;
 import java.sql.Timestamp;
 import java.util.function.Function;
@@ -21,26 +22,23 @@ public class OrderService {
     private final CustomerRepository customerRepository;
     private final ProductRepository productRepository;
     private final OrderItemRepository orderItemRepository;
+    private final Estimation estimation;
 
-    public OrderService(OrderRepository orderRepository, CustomerRepository customerRepository, ProductRepository productRepository, OrderItemRepository orderItemRepository) {
+
+    public OrderService(OrderRepository orderRepository, CustomerRepository customerRepository,
+                        ProductRepository productRepository, OrderItemRepository orderItemRepository,
+                        InventoryRepository inventoryRepository, Estimation estimation) {
         this.orderRepository = orderRepository;
         this.customerRepository = customerRepository;
         this.productRepository = productRepository;
         this.orderItemRepository = orderItemRepository;
+        this.estimation = estimation;
     }
 
     public List<ShowOrderDTO> returnAllOrders() {
         List<ShowOrderDTO> orders = new ArrayList<>();
 
 
-        /*
-
-            id BIGSERIAL PRIMARY KEY,
-            order_id BIGINT REFERENCES customer_order(id) ON DELETE CASCADE,
-            product_id BIGINT REFERENCES product(id),
-            qty NUMERIC(10,2) NOT NULL
-
-         */
 
         for (Order order : orderRepository.findAll()) {
             List<OrderItemDTO> orderItemDTOList = new ArrayList<>();
@@ -58,17 +56,13 @@ public class OrderService {
 
     public ShowOrderDTO returnOrderById(Long id) {
         Order order = orderRepository.findById(id).orElseThrow();
-
         List<OrderItemDTO> orderItemDTOList = new ArrayList<>();
         List<OrderItem> aa = orderItemRepository.findAllByOrderId(order.getId());
-
         for (OrderItem order1 : aa) {
             orderItemDTOList.add(new OrderItemDTO(order1.getId(), new ProductCreateDTO(order1.getProduct().getName(), order1.getProduct().getDescription()), order1.getQty()));
         }
 
-
         return new ShowOrderDTO(order, orderItemDTOList);
-
     }
 
     public Order saveOrderToDB(@NonNull List<CreateItemsInOrderDTO> createItemsInOrderDTOList, Long customerId) {
@@ -81,8 +75,14 @@ public class OrderService {
                 new Timestamp(rightNow.getTimeInMillis())
         );
         Order saved = orderRepository.saveAndFlush(order);
-        addItemsToOrder(saved.getId(),createItemsInOrderDTOList);
-        return saved;
+        addItemsToOrder(saved.getId(), createItemsInOrderDTOList);
+
+        List<OrderItem> orderItems = orderItemRepository.findAllByOrderId(saved.getId());
+
+        saved.setPlannedStartAt(estimation.estimate(orderItems).getFirst());
+        saved.setPlannedEndAt(estimation.estimate(orderItems).getLast());
+
+        return orderRepository.saveAndFlush(saved);
     }
 
 
@@ -91,8 +91,8 @@ public class OrderService {
         List<OrderItem> currentItems = orderItemRepository.findAllByOrderId(orderId);
         List<OrderItem> orderItemList = new ArrayList<>();
         OrderStatus[] blockedStatuses = {OrderStatus.CANCELLED, OrderStatus.COMPLETED, OrderStatus.IN_PRODUCTION};
-        if (Arrays.stream(blockedStatuses).toList().contains(order.getOrderStatus()))
-            throw new IllegalOperationException("Cannot add items to this order because of its status: " + order.getOrderStatus());
+        if (Arrays.stream(blockedStatuses).toList().contains(order.getStatus()))
+            throw new IllegalOperationException("Cannot add items to this order because of its status: " + order.getStatus());
 
 
         Map<Long, OrderItem> currentItemsMap = currentItems.stream().collect(Collectors.toMap(orderItem -> orderItem.getProduct().getId(), Function.identity()));
@@ -123,8 +123,8 @@ public class OrderService {
     public void deleteItemsFromOrder(Long orderId, Long id){
         Order order = orderRepository.findById(orderId).orElseThrow();
         OrderStatus[] blockedStatuses = {OrderStatus.CANCELLED, OrderStatus.COMPLETED, OrderStatus.IN_PRODUCTION};
-        if (Arrays.stream(blockedStatuses).toList().contains(order.getOrderStatus()))
-            throw new IllegalOperationException("Cannot delete items from this order because of its status: " + order.getOrderStatus());
+        if (Arrays.stream(blockedStatuses).toList().contains(order.getStatus()))
+            throw new IllegalOperationException("Cannot delete items from this order because of its status: " + order.getStatus());
         if(id == null) orderItemRepository.deleteAllByOrderId(orderId);
         else orderItemRepository.deleteById(id);
     }
@@ -132,8 +132,8 @@ public class OrderService {
     public void updateItemInOrder(Long orderId, Long id, Double qty) {
         Order order = orderRepository.findById(orderId).orElseThrow();
         OrderStatus[] blockedStatuses = {OrderStatus.CANCELLED, OrderStatus.COMPLETED, OrderStatus.IN_PRODUCTION};
-        if (Arrays.stream(blockedStatuses).toList().contains(order.getOrderStatus()))
-            throw new IllegalOperationException("Cannot change items in this order because of its status: " + order.getOrderStatus());
+        if (Arrays.stream(blockedStatuses).toList().contains(order.getStatus()))
+            throw new IllegalOperationException("Cannot change items in this order because of its status: " + order.getStatus());
 
         OrderItem orderItem = orderItemRepository.findById(id).orElseThrow();
         if(qty.isNaN() || qty==0 || qty < 0) throw new WrongValueException("Did you mean delete?");
