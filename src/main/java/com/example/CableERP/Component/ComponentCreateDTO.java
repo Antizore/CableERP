a814a1b8f26f2c30ed850f8673b98e8684e30536
Antizore <2906pascal@gmail.com>
2026-02-05 23:30:32 +0100

    add validation size

--- src/main/java/com/example/CableERP/Component/ComponentCreateDTO.java
+++ src/main/java/com/example/CableERP/Component/ComponentCreateDTO.java
@@ -2,9 +2,10 @@ package com.example.CableERP.Component;
 
 import com.example.CableERP.Common.Unit;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ComponentCreateDTO(
-        @NotNull String name,
+        @NotNull @Size(min = 2) String name,
         @NotNull Unit unit,
         @NotNull Double costPerUnit) {
 }
