6740babf71b0d6d1fb35375ab470ee917454b2fe
Antizore <2906pascal@gmail.com>
2026-02-05 23:24:37 +0100

    adding annotation

--- src/main/java/com/example/CableERP/Component/ComponentCreateDTO.java
+++ src/main/java/com/example/CableERP/Component/ComponentCreateDTO.java
@@ -1,6 +1,10 @@
 package com.example.CableERP.Component;
 
 import com.example.CableERP.Common.Unit;
+import jakarta.validation.constraints.NotNull;
 
-public record ComponentCreateDTO(String name, Unit unit, Double costPerUnit) {
+public record ComponentCreateDTO(
+        @NotNull String name,
+        @NotNull Unit unit,
+        @NotNull Double costPerUnit) {
 }
