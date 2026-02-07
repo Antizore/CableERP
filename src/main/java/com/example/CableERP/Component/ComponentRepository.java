78d87c70225f32956c85f847d82d4b0fc1657b9f
Antizore <2906pascal@gmail.com>
2026-02-07 11:20:01 +0100

    making Optional for better empty results handling in services

--- src/main/java/com/example/CableERP/Component/ComponentRepository.java
+++ src/main/java/com/example/CableERP/Component/ComponentRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ComponentRepository extends JpaRepository<Component, Long> {
-    Component findByName (@Param("name") String name);
+    Optional<Component> findByName (@Param("name") String name);
 }
