9fa575d8d223b4d13b65229c079db3cad7e541a1
Antizore <2906pascal@gmail.com>
2026-02-07 11:19:23 +0100

    making component create dto more meaningful and adding simple constraints handled by jakarta

--- src/main/java/com/example/CableERP/Component/ComponentCreateDTO.java
+++ src/main/java/com/example/CableERP/Component/ComponentCreateDTO.java
@@ -1,11 +1,19 @@
 package com.example.CableERP.Component;
 
 import com.example.CableERP.Common.Unit;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record ComponentCreateDTO(
-        @NotNull @Size(min = 2) String name,
-        @NotNull Unit unit,
-        @NotNull Double costPerUnit) {
+        @NotNull(message = "Component name is required")
+        @Size(min = 2, message = "Component name needs to have 2 or more characters")
+        String name,
+        @NotNull (message = "Component unit is required")
+        Unit unit,
+        @NotNull (message = "Cost per unit is required")
+        @Digits(integer =10, fraction=2, message = "Currently supported (10,2)")
+        @Positive(message = "The price must be greater than 0")
+        Double costPerUnit) {
 }
