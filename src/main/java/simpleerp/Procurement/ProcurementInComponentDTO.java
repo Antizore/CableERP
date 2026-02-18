c268d6c9787de4ac0d64853af8897f90efd545da
Antizore <2906pascal@gmail.com>
2026-02-18 22:39:13 +0100

    why were you there

--- src/main/java/simpleerp/Procurement/ProcurementInComponentDTO.java
+++
@@ -1,4 +0,0 @@
-package simpleerp.Procurement;
-
-public record ProcurementInComponentDTO(String name, String email, Double leadTimeDays) {
-}
--- src/main/java/simpleerp/Procurement/UpdateProcurementDTO.java
+++
@@ -1,3 +0,0 @@
-package simpleerp.Procurement;
-
-public record UpdateProcurementDTO(String name, String email, String phone, Double leadTimeDays) { }
