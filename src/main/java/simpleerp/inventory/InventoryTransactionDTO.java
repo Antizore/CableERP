b9e7a852d7c3b06a45e71c10a1eb51fade7284c5
Antizore <2906pascal@gmail.com>
2026-02-25 21:52:11 +0100

    adding constraints to dto

--- src/main/java/simpleerp/inventory/InventoryTransactionDTO.java
+++ src/main/java/simpleerp/inventory/InventoryTransactionDTO.java
@@ -1,6 +1,16 @@
 package simpleerp.inventory;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record InventoryTransactionDTO(
+
+        @NotNull
         Long componentId,
+
+        @NotNull
+        @Digits(integer =10, fraction=2, message = "Currently supported (10,2)")
+        @Min(value = 0, message = "qty cannot be less than 0")
         double qty
 ) {}
