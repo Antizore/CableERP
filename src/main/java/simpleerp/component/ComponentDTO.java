94cf5c191e9193e81bc965d58aa41f04efcec9cb
Antizore <2906pascal@gmail.com>
2026-02-25 21:53:05 +0100

    adding constraints to dto

--- src/main/java/simpleerp/component/ComponentDTO.java
+++ src/main/java/simpleerp/component/ComponentDTO.java
@@ -1,3 +1,13 @@
 package simpleerp.component;
 
-public record ComponentDTO(String name, String unit) {}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record ComponentDTO(
+        @NotNull(message = "component name is required")
+        @Size(min = 2, message = "component name needs to have 2 or more characters")
+        String name,
+
+        @NotNull (message = "component unit is required")
+        String unit
+) {}
