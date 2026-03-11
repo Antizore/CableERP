951ec2b3da50cec28753df05a996b842f41e5ee2
Antizore <2906pascal@gmail.com>
2026-03-11 22:56:45 +0100

    adding todo

--- src/main/java/simpleerp/product/ProductCreateDTO.java
+++ src/main/java/simpleerp/product/ProductCreateDTO.java
@@ -3,6 +3,7 @@ package simpleerp.product;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+//TODO: add minutesToProduce with corresponding constraints
 public record ProductCreateDTO(
         @NotNull
         @Size(min = 2, message = "product name needs to have 2 or more characters")
