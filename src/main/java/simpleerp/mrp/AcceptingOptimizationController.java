3fb0a607d241c0fd8aa0173e666efcd477bf85d6
Antizore <2906pascal@gmail.com>
2026-03-15 21:43:41 +0100

    laying the groundwork for accepting optimization

---
+++ src/main/java/simpleerp/mrp/AcceptingOptimizationController.java
@@ -0,0 +1,17 @@
+package simpleerp.mrp;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/optimization")
+public class AcceptingOptimizationController {
+
+
+
+
+
+
+
+}
---
+++ src/main/java/simpleerp/mrp/AcceptingOptimizationService.java
@@ -0,0 +1,8 @@
+package simpleerp.mrp;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AcceptingOptimizationService {
+}
