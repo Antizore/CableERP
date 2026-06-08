0fcb7ca10db9daac53ba1efd9f7f9ec0ff08aa4c
Antizore <2906pascal@gmail.com>
2026-06-08 08:16:22 +0200

    deleting unused function

--- src/main/java/simpleerp/bom/BillOfMaterialsRepository.java
+++ src/main/java/simpleerp/bom/BillOfMaterialsRepository.java
@@ -2,6 +2,7 @@ package simpleerp.bom;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,7 @@ public interface BillOfMaterialsRepository extends JpaRepository<BillOfMaterials
 
     List<BillOfMaterials> findAllByProduct_Id(@Param("product_id") Long productId);
     List<BillOfMaterials> findAllByComponent_Id(@Param("component_id") Long componentId);
-    List<BillOfMaterials> findAllByProduct_Name(String name);
+
 
 
 }
